d5aef5c759e2c0f33c701ebedfe74e2fb03cdd76
Eduardo Brito <eduardo370@outlook.pt>
2024-12-06 16:40:41 +0100

    slide de imagens na home

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,40 +1,86 @@
-import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, { useState } from 'react';
+import { StyleSheet, Dimensions, Image, SafeAreaView, FlatList, View } from 'react-native';
+
+const {width} = Dimensions.get('window');
+
+const images = [
+  {
+    id: 1,
+    url: 'https://picsum.photos/id/1/500/500'
+  },
+  {
+    id: 2,
+    url: 'https://picsum.photos/id/2/500/500'
+  },
+  {
+    id: 3,
+    url: 'https://picsum.photos/id/3/500/500'
+  },
+  {
+    id: 4,
+    url: 'https://picsum.photos/id/4/500/500'
+  }
+]
+
+const OnBoardingItem = ({item}:any) => {
+  return(
+    <Image source={{uri: item.url}} style={styles.image} />
+  )
+}
 
 export default function Home() {
-  const [email, setEmail] = useState('');
 
-  useEffect(() => {
-    const fetchEmail = async () => {
-      const storedEmail = await AsyncStorage.getItem('userEmail');
-      setEmail(storedEmail || 'Usuário não autenticado');
-    };
-
-    fetchEmail();
-  }, []);
+  const [activeIndex, setActiveIndex] = useState(0);
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Bem-vindo!</Text>
-      <Text style={styles.email}>Email: {email}</Text>
-    </View>
+    <SafeAreaView>
+      <FlatList 
+        data={images}
+        style={{maxHeight:width}}
+        pagingEnabled
+        horizontal
+        scrollEventThrottle={16}
+        onMomentumScrollEnd={(event) => {
+          setActiveIndex(event.nativeEvent.contentOffset.x/width)
+        }}
+        showsHorizontalScrollIndicator={false}
+        keyExtractor={(item) => String(item?.id)}
+        renderItem={({item}) => <OnBoardingItem item={item} />}
+      />
+      {
+        images.length > 1 ? 
+          <View style={styles.dotsContainer}>
+            {
+              images.map((_, i) => (
+                <View style={[styles.dot, {backgroundColor: i === activeIndex ? 'blue' : 'grey'}]}>
+
+                </View>
+              ))
+            }
+          </View>
+        : null
+      }
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  image: {
+    width,
+    height: 200,
+  },
+  dotsContainer: {
+    flexDirection: 'row',
     justifyContent: 'center',
-    alignItems: 'center',
-    padding: 16,
+    marginTop: 10
   },
-  title: {
-    fontSize: 24,
-    marginBottom: 16,
-  },
-  email: {
-    fontSize: 18,
-    color: '#333',
+  dot: {
+    width: 8,
+    height: 8,
+    borderRadius: 4,
+    marginHorizontal: 1 
   },
 });
