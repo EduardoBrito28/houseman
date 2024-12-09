e552956aa1307868d34982762ecbc5e7e260774d
Eduardo Brito <eduardo370@outlook.pt>
2024-12-09 17:43:48 +0100

    category grid

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,7 +1,8 @@
+import { Ionicons } from '@expo/vector-icons';
 import React, { useState } from 'react';
-import { StyleSheet, Dimensions, Image, SafeAreaView, FlatList, View } from 'react-native';
+import { StyleSheet, Dimensions, Image, SafeAreaView, FlatList, View, TouchableOpacity, Text } from 'react-native';
 
-const {width} = Dimensions.get('window');
+const { width } = Dimensions.get('window');
 
 const images = [
   {
@@ -22,9 +23,9 @@ const images = [
   }
 ]
 
-const OnBoardingItem = ({item}:any) => {
-  return(
-    <Image source={{uri: item.url}} style={styles.image} />
+const OnBoardingItem = ({ item }: any) => {
+  return (
+    <Image source={{ uri: item.url }} style={styles.image} />
   )
 }
 
@@ -33,54 +34,108 @@ export default function Home() {
   const [activeIndex, setActiveIndex] = useState(0);
 
   return (
-    <SafeAreaView>
-      <FlatList 
-        data={images}
-        style={{maxHeight:width}}
-        pagingEnabled
-        horizontal
-        scrollEventThrottle={16}
-        onMomentumScrollEnd={(event) => {
-          setActiveIndex(event.nativeEvent.contentOffset.x/width)
-        }}
-        showsHorizontalScrollIndicator={false}
-        keyExtractor={(item) => String(item?.id)}
-        renderItem={({item}) => <OnBoardingItem item={item} />}
-      />
-      {
-        images.length > 1 ? 
-          <View style={styles.dotsContainer}>
-            {
-              images.map((_, i) => (
-                <View style={[styles.dot, {backgroundColor: i === activeIndex ? 'blue' : 'grey'}]}>
+    <SafeAreaView className='flex-1 bg-white'>
+      <View className='bg-white flex flex-col justify-start items-start'>
+        <FlatList
+          data={images}
+          className='relative'
+          pagingEnabled
+          horizontal
+          scrollEventThrottle={16}
+          onMomentumScrollEnd={(event) => {
+            setActiveIndex(event.nativeEvent.contentOffset.x / width)
+          }}
+          showsHorizontalScrollIndicator={false}
+          keyExtractor={(item) => String(item?.id)}
+          renderItem={({ item }) => <OnBoardingItem item={item} />}
+        />
 
-                </View>
-              ))
-            }
+        <View className='flex flex-row justify-center absolute bottom-5 left-0 right-0'>
+          {
+            images.map((item, index) => (
+              <View
+                key={item.id}
+                className={` h-2 rounded-full mx-1  ${index === activeIndex ? 'w-5 bg-[#5F60B9]' : 'w-2 bg-white'}`}></View>
+            ))
+          }
+        </View>
+
+        <View className='w-full flex-1 flex pr-3 items-end absolute top-3 right-0'>
+          <TouchableOpacity className='bg-white rounded-full w-12 h-12 flex justify-center items-center'>
+            <Ionicons name="notifications-outline" size={23} color="#5F60B9" />
+          </TouchableOpacity>
+        </View>
+      </View>
+
+      <View className='flex flex-col w-full gap-5 p-5'>
+        <View className='flex flex-row items-center justify-between w-full'>
+          <Text className='text-lg font-bold'>Category</Text>
+          <TouchableOpacity>
+            <Text className='text-lg font-normal text-gray-500'>View All</Text>
+          </TouchableOpacity>
+        </View>
+        <View className='flex flex-col w-full gap-3'>
+
+          <View className='w-full flex flex-row gap-3 items-center justify-between'>
+
+            <View className='bg-gray-200 flex-1 justify-between items-center h-36 rounded-xl border border-gray-300'>
+              <View className='w-full flex-1 flex justify-center items-center'>
+                <Image source={require('../../assets/plumber.png')} className='w-12 h-12 ' />
+              </View>
+              <Text className='bg-white rounded-b-xl p-2 font-medium w-full text-center'>Plumber</Text>
+            </View>
+
+            <View className='bg-gray-200 flex-1 justify-between items-center h-36 rounded-xl border border-gray-300'>
+              <View className='w-full flex-1 flex justify-center items-center'>
+                <Image source={require('../../assets/carpenter.png')} className='w-12 h-12 ' />
+              </View>
+              <Text className='bg-white rounded-b-xl p-2 font-medium w-full text-center'>Carpenter</Text>
+            </View>
+
+            <View className='bg-gray-200 flex-1 justify-between items-center h-36 rounded-xl border border-gray-300'>
+              <View className='w-full flex-1 flex justify-center items-center'>
+                <Image source={require('../../assets/painter.png')} className='w-12 h-12 ' />
+              </View>
+              <Text className='bg-white rounded-b-xl p-2 font-medium w-full text-center'>Painting</Text>
+            </View>
+
           </View>
-        : null
-      }
+
+          <View className='w-full flex flex-row gap-3 items-center justify-between'>
+
+            <View className='bg-gray-200 flex-1 justify-between items-center h-36 rounded-xl border border-gray-300'>
+              <View className='w-full flex-1 flex justify-center items-center'>
+                <Image source={require('../../assets/salon.png')} className='w-12 h-12 ' />
+              </View>
+              <Text className='bg-white rounded-b-xl p-2 font-medium w-full text-center'>Salon</Text>
+            </View>
+
+            <View className='bg-gray-200 flex-1 justify-between items-center h-36 rounded-xl border border-gray-300'>
+              <View className='w-full flex-1 flex justify-center items-center'>
+                <Image source={require('../../assets/smart-home.png')} className='w-12 h-12 ' />
+              </View>
+              <Text className='bg-white rounded-b-xl p-2 font-medium w-full text-center'>Smart Home</Text>
+            </View>
+
+            <View className='bg-gray-200 flex-1 justify-between items-center h-36 rounded-xl border border-gray-300'>
+              <View className='w-full flex-1 flex justify-center items-center'>
+                <Image source={require('../../assets/ac-repair.png')} className='w-12 h-12 ' />
+              </View>
+              <Text className='bg-white rounded-b-xl p-2 font-medium w-full text-center'>AC Repair</Text>
+            </View>
+
+          </View>
+
+        </View>
+      </View>
+
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
   image: {
     width,
-    height: 200,
-  },
-  dotsContainer: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    marginTop: 10
-  },
-  dot: {
-    width: 8,
-    height: 8,
-    borderRadius: 4,
-    marginHorizontal: 1 
-  },
+    height: 230,
+  }
 });
